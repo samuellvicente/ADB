0bee89c96349559cd391723b567c89199c782565
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2019-10-17 00:48:15 +0200

    Report, updated ERD and schema

--- Transactions_1_Delivery.sql
+++ Transactions_1_Delivery.sql
@@ -1,1 +1,8 @@
--- Description: Withdraw money from a savings account. To do this we first must check if the Duration of the Savings is smaller than the current date, if it is the money withdrawn should be the amount put in plus the interest on that amount, if its bigger than only the amount. The money firt must go to the current account and than a Log should be made in the Withdraw table signifiying the operation, and taken from the currennt.
+-- Description: 
+--    Withdraw money from a savings account. To do this we first must check if the Duration of the Savings is smaller than the current date, if it is the money withdrawn should be the amount put in plus the interest on that amount, if its bigger then we should not be able to do it. The money firt must go to the current account and than a Log should be made in the Withdraw table signifying the operation, and taken from the current.
+
+begin transaction
+
+  insert blablabla...
+
+
