cf155e0365fec9be14b80f06089f8594bfa6ec5f
samuellvicente <samuel.vicente@tecnico.ulisboa.pt>
2019-11-06 02:05:46 +0100

    cleaned files tree

--- populate/generator.py
+++
@@ -1,9 +0,0 @@
-with open("./resources/firstNames.txt", 'r', encoding='utf-8') as fName, open("./resources/surnames.txt", 'r', encoding='utf-8') as sName:
-    fnames=fName.read().splitlines()
-    snames=sName.read().splitlines()
-    for surname in snames:
-        for name in fnames:
-            fullName=name.strip()+" "+surname.strip()
-            print(fullName)
-
-
--- populate/test.py
+++
@@ -1,4 +0,0 @@
-import random
-with open("./resources/citiesCountries.txt", 'r', encoding='utf-8') as cities:
-    print(cities.read().splitlines())
-
